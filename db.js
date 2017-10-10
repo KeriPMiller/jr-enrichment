c2ed94e42fd02729f93100c3534501a5bf0dcc49
KeriPMiller <miller.keripatricia@gmail.com>
2017-10-09 21:44:09 -0400

    added teacher and student models

--- db.js
+++ db.js
@@ -8,12 +8,38 @@ const db = new Sequelize('postgres://localhost/juniorenrichment', {
 
 const Student = db.define("student" , {
 	/* STUDENT MODEL CODE HERE */
-
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  gpa : {
+    type: Sequelize.FLOAT
+  },
+  grade : {
+    type: Sequelize.VIRTUAL,
+    get () {
+      return ['F', 'D', 'C', 'B', 'A'][Math.floor(this.gpa)]
+    }
+  }
 })
-	
+
+
+Student.findPerfectScores = () => {
+   return Student.findAll({
+    where: {grade: 'A'}
+  });
+}
+
 
 const Teacher = db.define('teacher', {
 	/* TEACHER MODEL CODE HERE */
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  subject:{
+    type: Sequelize.STRING,
+  }
 
 });
 
